cc46b70ba32b249c1fc70bbd72eac7810e47a5b8
Kalem <kalem.smith@student.nmt.edu>
2023-08-28 14:30:54 -0600

    trying stb_image

--- read_image/image.c
+++ read_image/image.c
@@ -1,14 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-	FILE* fptr;
-	fptr = fopen("test.txt", "r");
-	
-	char myString[101];
-	fgets(myString, 2, fptr);
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image/stb_image.h"
+const size_t NUM_PIXELS_TO_PRINT = 10U;
 
-	printf("%s", myString);
-	fclose(fptr);
+
+int main() {
+	int width, height, comp;
+	unsigned char *data = stbi_load("test.png", &width, &height, &comp, 0);
+	if (data) {
+		printf("width = %d, height = %d, comp = %d (channels)\n", width, height, comp);
+		for (size_t i = 0; i < NUM_PIXELS_TO_PRINT * comp; i++) {
+			printf("%02x%s", data[i], ((i + 1) % comp) ? "" : "\n");
+		}
+		printf("\n");
+	}
 	return 0;
 }
