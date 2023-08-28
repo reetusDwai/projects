5a8b7e91fcdd4da6ccb6e86facae905203d4961a
Kalem <kalem.smith@student.nmt.edu>
2023-08-28 13:59:17 -0600

    reading txt file

--- read_image/image.c
+++ read_image/image.c
@@ -2,5 +2,13 @@
 
 int main()
 {
-	printf("works");
+	FILE* fptr;
+	fptr = fopen("test.txt", "r");
+	
+	char myString[101];
+	fgets(myString, 2, fptr);
+
+	printf("%s", myString);
+	fclose(fptr);
+	return 0;
 }
