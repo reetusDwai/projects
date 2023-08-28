23e6cb6c6145709820ccb8838378faa43f7a4ec6
Kalem <kalem.smith@student.nmt.edu>
2023-08-28 14:43:32 -0600

    image working

--- read_image/image.c
+++ read_image/image.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image/stb_image.h"
 const size_t NUM_PIXELS_TO_PRINT = 10U;
 
 
-int main() {
+int main(void) {
 	int width, height, comp;
-	unsigned char *data = stbi_load("test.png", &width, &height, &comp, 0);
+	unsigned char *data = stbi_load("test.jpg", &width, &height, &comp, 0);
 	if (data) {
 		printf("width = %d, height = %d, comp = %d (channels)\n", width, height, comp);
 		for (size_t i = 0; i < NUM_PIXELS_TO_PRINT * comp; i++) {
