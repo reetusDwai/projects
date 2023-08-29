c9c3492b51fb7d8c9c933def01c25c9f0801c92e
Kalem <kalem.smith@student.nmt.edu>
2023-08-29 09:47:15 -0600

    removed txt file

--- read_image/image.c
+++ read_image/image.c
@@ -6,13 +6,17 @@
 
 int main(void) {
 	int width, height, comp;
+	int img[height];
 	unsigned char *data = stbi_load("test.jpg", &width, &height, &comp, 0);
 	if (data) {
 		printf("width = %d, height = %d, comp = %d (channels)\n", width, height, comp);
-		for (int i = 0; i < width * height * comp; i++) {
-			printf("%02x%s", data[i], ((i + 1) % comp) ? "" : "\n");
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width * comp; j++) {
+				// prints each pixel as hexidecimal
+				printf("%02x%s", data[i*width+j], ((j + 1) % comp) ? "" : "\n"); 
+			}
+			printf("\n");
 		}
-		printf("\n");
 	}
 	return 0;
 }
