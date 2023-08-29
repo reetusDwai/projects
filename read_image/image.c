6c5305a8e34f449ca56867eb4969d019323f1f0b
Kalem <kalem.smith@student.nmt.edu>
2023-08-29 10:05:33 -0600

    moved image reading to new function

--- read_image/image.c
+++ read_image/image.c
@@ -4,19 +4,25 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image/stb_image.h"
 
-int main(void) {
+int[][] img_to_gray() {
 	int width, height, comp;
-	int img[height];
 	unsigned char *data = stbi_load("test.jpg", &width, &height, &comp, 0);
+	int img[height][width];
 	if (data) {
 		printf("width = %d, height = %d, comp = %d (channels)\n", width, height, comp);
 		for (int i = 0; i < height; i++) {
 			for (int j = 0; j < width * comp; j++) {
 				// prints each pixel as hexidecimal
 				printf("%02x%s", data[i*width+j], ((j + 1) % comp) ? "" : "\n"); 
+				// converts each pixel to a luminace value from 0 - 255;
+				img[i][j] = 0;
 			}
 			printf("\n");
 		}
 	}
-	return 0;
+	return img;
+}
+
+// Luminace = 0.299 R + 0.587 G + 0.114 B
+int main(void) {
 }
